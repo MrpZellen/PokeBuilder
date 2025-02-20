0a510b0ff800a74f155f78d198868ac43f71a1b7
HeffleyA <aheffley@student.neumont.edu>
2025-02-20 10:23:47 -0700

    Model Started

---
+++ PokeBuilderMAUI/PokeBuilderMAUI.Shared/Models/Pokemon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeBuilderMAUI.Shared.Models
+{
+    enum Type
+    {
+        TYPELESS,
+        NORMAL,
+        GRASS,
+        WATER,
+        FIRE,
+        BUG,
+        GROUND,
+        ROCK,
+        FLYING,
+        FIGHTING,
+        PSYCHIC,
+        GHOST,
+        STEEL,
+        DARK,
+        DRAGON,
+        FAIRY,
+        ICE,
+        ELECTRIC,
+        POISON
+    }
+
+    class Pokemon
+    {
+        string? name { get; set; }
+        Type primary { get; set; }
+        Type secondary { get; set; } = Type.TYPELESS;
+        string? ability { get; set; }
+        List<string>? abilities { get; set; }
+        string[] moves = new string[4];
+        int baseHealth { get; set; }
+        int baseAttack { get; set; }
+        int baseDefense { get; set; }
+        int baseSpAttack { get; set; }
+        int baseSpDefense { get; set; }
+        int baseSpeed { get; set; }
+        string? image { get; set; }
+    }
+}
