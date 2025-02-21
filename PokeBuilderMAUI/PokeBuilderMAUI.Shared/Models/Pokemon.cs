f33d190f900edf1f37054034cb5e707ecf9dec84
HeffleyA <aheffley@student.neumont.edu>
2025-02-21 11:27:17 -0700

    MOVES

--- PokeBuilderMAUI/PokeBuilderMAUI.Shared/Models/Pokemon.cs
+++ PokeBuilderMAUI/PokeBuilderMAUI.Shared/Models/Pokemon.cs
@@ -32,20 +32,32 @@ namespace PokeBuilderMAUI.Shared.Models
     class Pokemon
     {
         private static string? name;
+        private static Type primary;
+        private static Type secondary = Type.TYPELESS;
+        private static string? ability;
+        private static List<string>? abilities;
+        private static List<string> moves;
+        private static string[] finalMoves = new string[4];
+        private static int baseHealth;
+        private static int baseAttack;
+        private static int baseDefense;
+        private static int baseSPAttack;
+        private static int baseSPDefense;
+        private static int baseSpeed;
+        private static string? image;
 
         public readonly string? Name = name;
-        public readonly Type Primary;
-        public readonly Type Secondary = Type.TYPELESS;
-        public readonly string? Ability;
-        public readonly List<string>? Abilities;
-        public readonly string[] Moves = new string[4];
-        public readonly int BaseHealth;
-        public readonly int BaseAttack;
-        public readonly int BaseDefense;
-        public readonly int BaseSpAttack;
-        public readonly int BaseSpDefense;
-        public readonly int BaseSpeed;
-        public readonly string? Image;
+        public readonly Type Primary = primary;
+        public readonly Type Secondary = secondary;
+        public readonly string? Ability = ability;
+        public readonly string[] Moves = finalMoves;
+        public readonly int BaseHealth = baseHealth;
+        public readonly int BaseAttack = baseAttack;
+        public readonly int BaseDefense = baseDefense;
+        public readonly int BaseSpAttack = baseSPAttack;
+        public readonly int BaseSpDefense = baseSPDefense;
+        public readonly int BaseSpeed = baseSpeed;
+        public readonly string? Image = image;
 
         Pokemon GetPokemon(string name, Type primary, Type secondary,
                             string ability, string[] moves, int baseHP,
