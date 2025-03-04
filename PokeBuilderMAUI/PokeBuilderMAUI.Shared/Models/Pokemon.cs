f4ea1bd5c8cb3ab6e7318f486b891e5ef84ab0ce
HeffleyA <aheffley@student.neumont.edu>
2025-03-04 10:03:15 -0700

    Pokemon Add Function

--- PokeBuilderMAUI/PokeBuilderMAUI.Shared/Models/Pokemon.cs
+++ PokeBuilderMAUI/PokeBuilderMAUI.Shared/Models/Pokemon.cs
@@ -3,7 +3,7 @@
 namespace PokeBuilderMAUI.Shared.Models
 {
     
-    enum Type
+    public enum Type
     {
         TYPELESS,
         NORMAL,
@@ -26,7 +26,7 @@ namespace PokeBuilderMAUI.Shared.Models
         POISON
     }
 
-    class Pokemon
+    public class Pokemon
     {
         public static List<Pokemon> AllPokemon { get; set; } = null;
         public Pokemon(bool isPopulated = true) {
@@ -36,8 +36,13 @@ namespace PokeBuilderMAUI.Shared.Models
                 Name = "Testasaur";
                 Primary = Type.GRASS;
                 Secondary = Type.TYPELESS;
-                Ability = "Overgrow";
-                Moves = ["Tackle", "Growl", "Leech Seed", "Vine Whip"];
+                Ability = null;
+                Abilities = new List<string> { "Overgrow", "Chlorophyll" };
+                Move1 = null;
+                Move2 = null;
+                Move3 = null;
+                Move4 = null;
+                Moves = new List<string> { "Tackle", "Razor Leaf", "Growl", "Leech Seed", "Vine Whip", "Poison Powder", "Sleep Powder", "Sweet Scent", "Growth", "Synthesis", "Solarbeam" };
                 BaseHealth = 45;
                 BaseAttack = 49;
                 BaseDefense = 49;
@@ -54,8 +59,13 @@ namespace PokeBuilderMAUI.Shared.Models
         public string? Name { get; set; }
         public Type Primary { get; private set; }
         public Type Secondary { get; private set; }
-        public string? Ability { get; private set; }
-        public string[] Moves { get; private set; }
+        public string? Ability { get; set; }
+        public List<string>? Abilities { get; set; }
+        public List<string> Moves { get; set; }
+        public string? Move1 { get; set; }
+        public string? Move2 { get; set; }
+        public string? Move3 { get; set; }
+        public string? Move4 { get; set; }
         public int BaseHealth { get; private set; }
         public int BaseAttack { get; private set; }
         public int BaseDefense { get; private set; }
@@ -65,7 +75,7 @@ namespace PokeBuilderMAUI.Shared.Models
         public string? Image { get; private set; }
 
         public Pokemon(string name, Type primary, Type secondary,
-                            string ability, string[] moves, int baseHP,
+                            string ability, List<string> moves, int baseHP,
                             int baseATK, int baseDEF, int baseSPATK,
                             int baseSPDEF, int baseSPD, string image)
         {
