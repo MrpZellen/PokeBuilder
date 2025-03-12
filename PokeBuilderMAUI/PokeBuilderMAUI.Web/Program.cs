8adb60dee94cb6053590f95d32efe603b96b0ef6
TheGreatRy <158355076+TheGreatRy@users.noreply.github.com>
2025-03-12 10:54:56 -0600

    SREANM

--- PokeBuilderMAUI/PokeBuilderMAUI.Web/Program.cs
+++ PokeBuilderMAUI/PokeBuilderMAUI.Web/Program.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa4a02fab1574ffa2919ae5102ef5728408d015deaa5211276970945abd3364b
-size 2919
+oid sha256:2096189908c2638cb30b72f9fbafbf33ec490acc3e7d68f8b1e22dc444829053
+size 2795
