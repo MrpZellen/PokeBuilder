80d0f858260e4413905f4a71529c23813d8eff97
DaikwonR <157632694+DaikwonR@users.noreply.github.com>
2025-03-12 13:49:37 -0600

    3/12

--- PokeBuilderMAUI/PokeBuilderMAUI.Shared/Services/UserService.cs
+++ PokeBuilderMAUI/PokeBuilderMAUI.Shared/Services/UserService.cs
@@ -15,7 +15,6 @@ namespace PokeBuilderMAUI.Shared.Services
     {
         private readonly IConfiguration _configuration;
         private readonly IMongoDatabase _database;
-        private readonly IMongoCollection<User> _userCollection;
 
         public UserService(IConfiguration configuration)
         {
@@ -25,22 +24,9 @@ namespace PokeBuilderMAUI.Shared.Services
             var mongoUrl = MongoUrl.Create(connectionString);
             var mongoClient = new MongoClient(mongoUrl);
             _database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
-
-            // MongoDB collection for users
-            _userCollection = _database.GetCollection<User>("UserInformationStorage");
         }
         public IMongoDatabase Database => _database;
 
-        public async Task<List<User>> GetUsersAsync()
-        {
-            return await _userCollection.Find(FilterDefinition<User>.Empty).ToListAsync();
-        }
-
-        public async Task<User> GetUserByUsernameAsync(string username)
-        {
-            var filter = Builders<User>.Filter.Eq(u => u.Username, username);
-            return await _userCollection.Find(filter).FirstOrDefaultAsync();
-        }
     }
 }
 >>>>>>> 9b4ef72 (fixed github)
