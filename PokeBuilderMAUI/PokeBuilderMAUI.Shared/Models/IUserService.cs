d15bc7ec52fd4ca7331850f483d94d87e632cf83
TheGreatRy <158355076+TheGreatRy@users.noreply.github.com>
2025-03-12 10:46:07 -0600

    e

--- PokeBuilderMAUI/PokeBuilderMAUI.Shared/Models/IUserService.cs
+++ PokeBuilderMAUI/PokeBuilderMAUI.Shared/Models/IUserService.cs
@@ -1,28 +1,3 @@
-<<<<<<< Updated upstream
 version https://git-lfs.github.com/spec/v1
-oid sha256:77872571cc3024883a3a1adc9ff029d6b50844efca3c3c199650ef41c34609d2
-size 621
-=======
-﻿using MongoDB.Bson;
-using PokeBuilderMAUI.Shared.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PokeBuilderMAUI.Shared.Services
-{
-    public interface IUserService
-    {
-        public Task<List<User>> GetAsync();
-        public Task<User?> GetAsync(string username);
-        public Task<User?> GetAsync(ObjectId id);
-        public Task CreateAsync(User newUser);
-        public Task UpdateAsync(User user, string userName);
-        public Task RemoveAsync(string userName);
-        public Task RemoveAsync(ObjectId id);
-
-    }
-}
->>>>>>> Stashed changes
+oid sha256:893ae487e071162e8352fcbfc991e66b5c3c81c1ad089207f109081ed37b98db
+size 807
--- PokeBuilderMAUI/PokeBuilderMAUI.Shared/Models/UserStorageDBContext.cs
+++ PokeBuilderMAUI/PokeBuilderMAUI.Shared/Models/UserStorageDBContext.cs
@@ -1,31 +1,3 @@
-<<<<<<< Updated upstream
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e71d554c763075c4541f4ea2c6e245549a61ba289d926a9fb559182280a9009
-size 696
-=======
-﻿using Microsoft.EntityFrameworkCore;
-using PokeBuilderMAUI.Shared.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.ConstrainedExecution;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PokeBuilderMAUI.Shared.Services
-{
-    public class UserStorageDBContext : DbContext
-    {
-       public DbSet<User> Users { get; init; }
-       public UserStorageDBContext(DbContextOptions<UserStorageDBContext> options) : base(options)
-        {
-
-        }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<User>();
-        }
-    }
-}
->>>>>>> Stashed changes
+oid sha256:9a354918823898bda64efea2215a9ad0c81709ebcd2a1ae339e1b0bf88f88f26
+size 881
