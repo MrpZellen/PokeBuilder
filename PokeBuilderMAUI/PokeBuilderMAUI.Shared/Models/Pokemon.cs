80643c3db754a777aed844b95a1abf511adfdc62
HeffleyA <aheffley@student.neumont.edu>
2025-03-13 10:11:02 -0600

    TEAM

--- PokeBuilderMAUI/PokeBuilderMAUI.Shared/Models/Pokemon.cs
+++ PokeBuilderMAUI/PokeBuilderMAUI.Shared/Models/Pokemon.cs
@@ -73,7 +73,8 @@ namespace PokeBuilderMAUI.Shared.Models
         public int BaseSpDefense { get; private set; }
         public int BaseSpeed { get; private set; }
         public string? Image { get; private set; }
-        public int NationalDexNumber { get; set; } 
+        public int NationalDexNumber { get; set; }
+        public bool ShouldRenderTeamMemberPartial { get; set; } = true;
 
         public Pokemon(string name, Type primary, Type secondary,
                             string ability, List<string> moves, int baseHP,
@@ -86,6 +87,17 @@ namespace PokeBuilderMAUI.Shared.Models
             BaseSpAttack = baseSPATK; BaseSpDefense = baseSPDEF;
             BaseSpeed = baseSPD; Image = image;
             NationalDexNumber = nationalDexNumber;
+        }
+
+        public Pokemon(string name, Type primary, Type secondary, string ability, 
+            string move1, string move2, string move3, string move4, 
+            int baseHP, int baseATK, int baseDEF, int baseSPATK, int baseSPDEF, int baseSPD, 
+            string image, int nationalDexNumber)
+        {
+            Name = name; Primary = primary; Secondary = secondary; ability = ability;
+            Move1 = move1; Move2 = move2; Move3 = move3; Move4 = move4;
+            BaseHealth = baseHP; BaseAttack = baseATK; BaseDefense = baseDEF; BaseSpAttack = baseSPATK; BaseSpDefense = baseSPDEF; BaseSpeed = baseSPD;
+            Image = image; NationalDexNumber = nationalDexNumber;
         }
         
     }
