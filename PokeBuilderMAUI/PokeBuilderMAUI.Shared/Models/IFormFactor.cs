d07b2bcc4b9e8cbb17be81cb199fc1c5107d841c
TheGreatRy <158355076+TheGreatRy@users.noreply.github.com>
2025-03-13 09:59:18 -0600

    wa

--- PokeBuilderMAUI/PokeBuilderMAUI.Shared/Models/IFormFactor.cs
+++ PokeBuilderMAUI/PokeBuilderMAUI.Shared/Models/IFormFactor.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47bb84e2d6a876d6d1e69062bc84f816b2c4a0342b8b467e3b6e49d2381ebab8
-size 352
+oid sha256:ac00c19565363e15a7c94d643c9185232f347f07fbd6bad0348075d08b42aba4
+size 168
--- PokeBuilderMAUI/PokeBuilderMAUI.Shared/Models/IUserService.cs
+++ PokeBuilderMAUI/PokeBuilderMAUI.Shared/Models/IUserService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:893ae487e071162e8352fcbfc991e66b5c3c81c1ad089207f109081ed37b98db
-size 807
+oid sha256:f613400cb544f01e0161af206d06661c34327f9f387eaf5fd76af427c18bff09
+size 623
